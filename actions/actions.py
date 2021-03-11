5a87eb91fc3318da87e99f29fb793047ebb14496
lakshmi25npathi <lakshmipathiae021@gmail.com>
2021-03-11 10:09:26 +0530

    changes in actions and activity recommendation nlu file

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,50 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+ from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+ from rasa_sdk import Action, Tracker
+ from rasa_sdk.executor import CollectingDispatcher
+
+# Outdoor activities list
+ class ActionOutdoorActivities(Action):
+
+     def name(self):
+         return "action_OutdoorActivities"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]):
+        
+         dispatcher.utter_message(template="utter_outdoor_activities")
+
+         return []
+
+
+# Indoor activities list
+ class ActionIndoorActivities(Action):
+
+     def name(self):
+         return "action_IndoorActivities"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]):
+        
+         dispatcher.utter_message(template="utter_indoor_activities")
+
+         return []
+
+
+# Local hikes list
+ class ActionLocalHikes(Action):
+
+     def name(self):
+         return "action_LocalHikes"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]):
+        
+         dispatcher.utter_message(template="utter_local_hikes")
+
+         return []
